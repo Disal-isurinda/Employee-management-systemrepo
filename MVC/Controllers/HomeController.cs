306fe6c964ba2301e28fb9d412db407470fdd8e2
LAPTOP-I6NU9M6C\asus <charanihasarapg@gmail.com>
2021-08-01 21:33:17 +0530

    added token authentication

--- MVC/Controllers/HomeController.cs
+++ MVC/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,11 +11,48 @@ namespace MVC.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
+        private static string WebAPIURL = "http://localhost:58108/";
+        
 
+        public async Task<ActionResult> IndexAsync()
+        {
+            var tokenBased = new HttpClient();
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Clear();
+                client.BaseAddress = new Uri(WebAPIURL);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var responseMessage = await client.GetAsync("Account/ValidLogin?userName=admin&userPassword = admin");
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    var resultMessage = responseMessage.Content.ReadAsStringAsync().Result;
+                    tokenBased = JsonCovert.DeserializeObject<string>(resultMessage);
+                    Session["TokenNumber"] = tokenBased;
+                    Session["UserName"] = "admin";
+
+                } 
+            }
+            return Content(tokenBased);
+
+        }
+        public async Task<ActionResult> GetEmployee()
+        {
+            string ReturnMessage = String.Empty;
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Clear();
+                client.BaseAddress = new Uri(WebAPIURL);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json");
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Session["TokenNumber"].ToString() + "Admin5");
+                var responseMessage = await client.GetAsync("Account/GetEmployee");
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    var resultMessage = responseMessage.Content.ReadAsStringAsync().Result;
+                    ReturnMessage = JsonConvert.DeserializeObject<string>(resultMessage);
+                }
+            }
+            return Content(ReturnMessage);
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -26,5 +66,10 @@ namespace MVC.Controllers
 
             return View();
         }
+    
+
+        private ActionResult Content(HttpClient tokenBased)
+        {
+            throw new NotImplementedException();
+        }
     }
-}
