33521f443895584e92ca371a32f467a444f6180b
LAPTOP-I6NU9M6C\asus <charanihasarapg@gmail.com>
2021-08-06 02:15:05 +0530

    removed unnecessary comments in Identity controller

--- MVC/Controllers/IdentityController.cs
+++ MVC/Controllers/IdentityController.cs
@@ -176,8 +176,6 @@ namespace MVC.Controllers
         #region Status Codes
         private static string ErrorCodeToString(MembershipCreateStatus createStatus)
         {
-            // See http://go.microsoft.com/fwlink/?LinkID=177550 for
-            // a full list of status codes.
             switch (createStatus)
             {
                 case MembershipCreateStatus.DuplicateUserName:
