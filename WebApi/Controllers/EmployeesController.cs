76d0463ff3ba722527c4aed9686beffafacb3333
LAPTOP-I6NU9M6C\asus <charanihasarapg@gmail.com>
2021-08-06 02:11:42 +0530

    added membership provider part 1

--- WebApi/Controllers/EmployeesController.cs
+++ WebApi/Controllers/EmployeesController.cs
@@ -11,6 +11,7 @@ using System.Web.Http.Description;
 using WebApi.Models;
 using System.Data.SqlClient;
 using System.Configuration;
+using System.Web.Security;
 
 namespace WebApi.Controllers
 {
@@ -77,6 +78,11 @@ namespace WebApi.Controllers
 
             db.Employees.Add(employee);
             db.SaveChanges();
+            string username = employee.FirstName + "." + employee.LastName;
+            string email = username + "@gmail.com";
+            string password = "ab@1234";
+            MembershipCreateStatus memcresta;
+            MembershipUser user = Membership.CreateUser(username, password, email, "question", "answer", true, out memcresta);
 
             return CreatedAtRoute("DefaultApi", new { id = employee.EmpID }, employee);
         }
